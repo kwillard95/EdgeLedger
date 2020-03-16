3c2a120a6b65f82743fa78a31518266a9c2ab370
kwillard95 <kwillard95@gmail.com>
2020-03-16 11:39:28 -0700

    Smooth scrolling and navbar styling

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ function initMap() {
   });
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Sticky menu opqaue background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+
+$('#navbar a, .btn').on('click', function(e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        // offsets scroll so that the title of the section is visible
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
